a5ea0101fa987d9dd0036b50ab0af1aefac2b776
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-04-10 23:27:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -331,7 +331,7 @@ st.set_page_config(page_title="Comparador Energético", layout="wide")
 
 # --- BLOQUE DEL LOGO ---
 if os.path.exists("Logo_Energetika.png"):
-    st.image("Logo_Energetika.png", width=370)
+    st.image("Logo_Energetika.png", width=320)
 
 st.title("⚡ Comparador Energetika de Facturas Eléctricas")
 
