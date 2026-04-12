de0668c593b4792453163720ae352c2ae47a84d3
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-04-12 20:01:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -473,7 +473,9 @@ else:
                         msg = f"Hola! He usado el comparador de Energetika y he visto que puedo ahorrar {ahorro_total}€ en {dias_totales} días (aprox. {ahorro_anual}€ al año) con la compañía {nombre_cia}. Me gustaría cambiarme."
                         url_whatsapp = f"https://wa.me/34614676150?text={msg.replace(' ', '%20')}"
 
-                        st.markdown(f'''<a href="{url_whatsapp}" target="_blank" class="whatsapp-button" style="background-color: {color_fondo}; color: #000000 !important; font-weight: 800; opacity: 1 !important; text-decoration: none;">
+                        st.markdown(f'''<a href="{url_whatsapp}" target="_blank" class="whatsapp-button" style="background-color: {color_fondo} !important; color: #000000 !important; 
+                        text-decoration: none !important; display: block !important; padding: 12px !important; text-align: center !important; border-radius: 8px !important; font-weight: bold !important;
+                        cursor: pointer !important;">
                         {texto_boton}</a>
                         ''', unsafe_allow_html=True)
 
