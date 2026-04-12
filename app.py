3f70093e5510171f58f88a732eaaf9efc8ce8009
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-04-13 00:30:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -471,7 +471,7 @@ else:
                                         text-align: left; 
                                         font-weight: bold; 
                                         font-size: 18px;
-                                        padding-left: 40px;
+                                        padding-left: 80px;
                                         margin-bottom: 15px; 
                                         color: {color_fondo};
                                     ">
