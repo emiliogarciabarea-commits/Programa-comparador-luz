21e03d59ee5940fde72f35a17d6e6e1c1aabb562
emiliogarciabarea-commits <emiliogarciabarea@gmail.com>
2026-04-26 18:40:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import streamlit as st
 import io
 import os
 
+# --- 2. LÓGICA DE KEEP-ALIVE (AQUÍ VA EL PING) ---
+if st.query_params.get("ping") == "true":
+    st.sidebar.success("Conexión activa (Ping recibido)")
+    st.stop() # Esto detiene la ejecución aquí para el robot, ahorrando recursos.
 def extraer_datos_factura(pdf_path):
     texto_completo = ""
     with pdfplumber.open(pdf_path) as pdf:
